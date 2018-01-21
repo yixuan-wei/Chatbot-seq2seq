bbe17ccd79c868fa0d9a53ee1f57d315046cda0a
yixuan-wei <yixuan_wei@qq.com>
2018-01-22 00:57:03 +0800

    gitignore_update

--- pull.sh
+++
@@ -1,4 +0,0 @@
-curl https://www.dropbox.com/s/ncfa5t950gvtaeb/test.enc?dl=0 -o test.enc --progress
-curl https://www.dropbox.com/s/48ro4759jaikque/test.dec?dl=0 -o test.dec --progress
-curl https://www.dropbox.com/s/gu54ngk3xpwite4/train.enc?dl=0 -o train.enc --progress
-curl https://www.dropbox.com/s/g3z2msjziqocndl/train.dec?dl=0 -o train.dec --progress
